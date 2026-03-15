f7515e09c2b121afdef08b83ba6bf1a4544419cc
Mika <baezamika@gmail.com>
2026-03-15 16:39:09 +0100

    feat: simplify connection card

--- src/lib/components/layout/ConnectionStatus.svelte
+++ src/lib/components/layout/ConnectionStatus.svelte
@@ -9,45 +9,48 @@ import Icon from "@iconify/svelte";
 
 let bluetoothContext = getBluetoothContext()
 </script>
+
+
+
 {#snippet bluetoothCompatibility()}
 	{@const textColor = bluetoothContext.isSupported ? 'dark:text-green-400 text-green-500' : 'dark:text-red-400 text-red-500'}
-	<div class="ml-1 flex gap-1 items-center">
-		<Icon icon="mingcute:{bluetoothContext.isSupported ? 'check' : 'close'}-circle-fill" class="size-3 {textColor}"/>
-		<CardDescription
-			class="{textColor} text-sm"
-		>{bluetoothContext.isSupported ? "Bluetooth supported" : "Bluetooth not supported"}</CardDescription>
+	<div class=" flex gap-1 items-center">
+		<Icon icon="mingcute:{bluetoothContext.isSupported ? 'check' : 'close'}-circle-fill" class="size-5 {textColor}"/>
 	</div>
 {/snippet}
-<Card class="fixed z-10 top-4 left-4 w-full max-w-sm p-2 px-0">
-	<CardContent class="px-2 flex flex-col gap-2">
-		{@render bluetoothCompatibility()}
-		{#if !bluetoothContext.isConnected}
-			<Button
-				class="w-full"
-				variant="secondary"
-				disabled={!bluetoothContext.isSupported}
-				onclick={() => {
+
+
+
+<Card class="fixed z-10 top-4 left-4 max-w-screen p-0.5 rounded-full">
+	<div
+		class="px-0 flex gap-1 items-center justify-between"
+	>
+		<div class="flex gap-1 items-center">
+			{@render bluetoothCompatibility()}
+			{#if (bluetoothContext.isConnected)}
+				<span class="text-sm font-medium mr-2">{bluetoothContext.deviceName}</span>
+			{/if}
+		</div>
+		{#if (!bluetoothContext.isConnected)}
+			<div
+				class="p-1 cursor-pointer bg-foreground/15 rounded-full"
+				on:click={() => {
 					console.log("Connecting")
 					bluetoothContext.connect()
 				}}
-			><Icon icon="mingcute:bluetooth-line"/>Connection
-			</Button>
+			>
+				<Icon icon="mingcute:add-fill" class="size-3"/>
+			</div>
 		{:else}
-			<div class="w-full flex justify-between items-center">
-				<div class="flex items-center gap-2 p-1.5 bg-muted rounded-sm w-full">
-					<Icon icon="mingcute:game-2-fill" class="size-5"/>
-					<span class="text-sm font-medium">{bluetoothContext.deviceName}</span>
-				</div>
-<!--				<Button-->
-<!--					variant="ghost"-->
-<!--					disabled={!bluetoothContext.isConnected}-->
-<!--					class="text-destructive"-->
-<!--					onclick={() => {-->
-<!--					console.log("Connecting")-->
-<!--					bluetoothContext.disconnect()-->
-<!--				}}-->
-<!--				><Icon icon="mingcute:user-x-line"/></Button>-->
+			<div
+				class="p-1 cursor-pointer bg-foreground/15 rounded-full"
+				on:click={() => {
+					console.log("Connecting")
+					bluetoothContext.disconnect()
+				}}
+			>
+				<Icon icon="mingcute:close-fill" class="size-3"/>
 			</div>
 		{/if}
-	</CardContent>
+	</div>
 </Card>
